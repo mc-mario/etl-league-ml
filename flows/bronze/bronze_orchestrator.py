95ea53126f5f98b294484918672c903fd8ab29ee
Mario M <mariobluemind@gmail.com>
2024-08-01 01:14:58 +0200

    Update bronze orchestrator to retrieve divisions daily and add cron schedule

--- flows/bronze/bronze_orchestrator.py
+++ flows/bronze/bronze_orchestrator.py
@@ -8,9 +8,10 @@ async def orchestrate_daily_division_retrieval():
         name='list-division-players/list_division_players'
     )
 
-    for divisions in ['I', 'II', 'III', 'IV']:
+    for division in ['I', 'II', 'III', 'IV']:
         parameters = dict(
-            tier='DIAMOND', division='I', queue='RANKED_SOLO_5x5'
+            tier='DIAMOND', division=division, queue='RANKED_SOLO_5x5'
         )
         await run_deployment(deployment.id, parameters=parameters)
 
+    ### Leer las divisiones del día y hacer una búsqueda de los summonerIds
