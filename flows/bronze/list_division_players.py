2bfef9ddb1165f18db48b4f60ed1b707cfd26ce6
Mario M <mariobluemind@gmail.com>
2024-07-31 21:47:00 +0200

    Pass env vars

--- flows/bronze/list_division_players.py
+++ flows/bronze/list_division_players.py
@@ -3,13 +3,14 @@ import os
 from datetime import datetime
 
 from prefect import task, flow
+from prefect.variables import Variable
 from pulsefire.clients import RiotAPIClient
 
-API_KEY = os.getenv('RIOT_API_KEY')
 
 @task
 async def get_division(tier='DIAMOND', division='I', queue='RANKED_SOLO_5x5'):
-    async with RiotAPIClient(default_headers={"X-Riot-Token": API_KEY}) as client:
+    API_KEY = Variable.get('riot_api_key')
+    async with RiotAPIClient(default_headers={"X-Riot-Token": API_KEY.value}) as client:
         return await client.get_lol_league_v4_entries_by_division(tier=tier, division=division, queue=queue, region='euw1')
 
 
