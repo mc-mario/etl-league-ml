ce75d7f5886d513792de02a75083caccd8f8c72b
Mario M <mariobluemind@gmail.com>
2024-07-31 10:58:48 +0200

    Update deployment and requirements.txt

--- create-deployment.py
+++ create-deployment.py
@@ -1,18 +1,17 @@
 from prefect.deployments import Deployment
-from prefect.filesystems import LocalFileSystem, GitHub
-from hello_world_flow import hello_world_flow
-
+from prefect_github import GitHubCredentials
 from prefect_github.repository import GitHubRepository
+
+from hello_world_flow import hello_world_flow
 
 github_repository_block = GitHubRepository.load("etl-league-ml")
 
+# Create a deployment for the hello_world_flow
 deployment = Deployment.build_from_flow(
     flow=hello_world_flow,
     name="hello-world-deployment",
+    parameters={"filename": "hello_world.txt"},
     storage=github_repository_block,
-    path="hello_world_flow.py",
-    work_queue_name="default",
-    infra_overrides={"env": {"PREFECT_LOGGING_LEVEL": "DEBUG"}},
 )
 
 if __name__ == "__main__":
