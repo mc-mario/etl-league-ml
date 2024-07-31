dbb0173b6ca9ddd15022b0d7c44db1c04a84e0f6
Mario M <mariobluemind@gmail.com>
2024-07-31 21:59:25 +0200

    Debug

--- flows/bronze/list_division_players.py
+++ flows/bronze/list_division_players.py
@@ -5,6 +5,8 @@ from prefect import task, flow
 from prefect.variables import Variable
 from pulsefire.clients import RiotAPIClient
 
+import os
+
 
 @task
 async def get_division(tier='DIAMOND', division='I', queue='RANKED_SOLO_5x5'):
@@ -19,5 +21,10 @@ async def list_division_players():
     division = 'I'
     queue = 'RANKED_SOLO_5x5'
     resp = await get_division(tier=tier, division=division, queue=queue)
+    print(os.listdir())
+    print(os.listdir('/opt'))
+    print(os.listdir('/opt/prefect'))
+    print(os.listdir('/opt/prefect/flows'))
+    print(os.listdir('/opt/prefect/flows/bronze'))
     with open(f'/opt/prefect/flows/bronze/division/{tier}_{division}_{queue}_{date.today()}.json', 'w') as f:
         json.dump(resp, f)
