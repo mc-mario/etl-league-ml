677f3ae80f841f41be762dc40111f76e00c4b267
Mario M <mariobluemind@gmail.com>
2024-08-12 23:44:24 +0200

    Persist silver etl status in db

--- flows/silver/silver_orchestrator.py
+++ flows/silver/silver_orchestrator.py
@@ -18,12 +18,12 @@ async def orchestrate_silver_etl(match_id=None, frame=15):
     )
 
     logger.info(f'Running process-match-details')
-    is_processable = await run_deployment(
+    run = await run_deployment(
         process_match_details.id,
         parameters={'match_id': match_id}
     )
-    logger.info(f'Result of is_processable={is_processable}')
-    if not is_processable:
+    logger.info(f'Result of is_processable={run}')
+    if not run:
         logger.info(f'{match_id} is not processable, marking as deleted')
         complete_step(session, match_id, 'is_deleted', True)
         return
@@ -36,3 +36,5 @@ async def orchestrate_silver_etl(match_id=None, frame=15):
         process_match_timeline.id,
         parameters={'match_id': match_id, 'frame': frame}
     )
+    logger.info(f'Marking {match_id} silver step')
+    complete_step(session, match_id, 'silver', True)
