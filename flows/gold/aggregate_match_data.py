5a32a0f5c81bef81318b4ad888b352fa88a8bae9
Mario M <mariobluemind@gmail.com>
2024-08-22 00:38:35 +0200

    Wrapping up code

--- flows/gold/aggregate_match_data.py
+++ flows/gold/aggregate_match_data.py
@@ -1,19 +1,22 @@
-import asyncio
+import os
 
 import pandas as pd
+from hyperframe.frame import FRAMES
+from prefect import task
 from prefect.variables import Variable
 
-ENTITY_DETAILS = 'match/details'
-ENTITY_TIMELINE = 'match/timeline'
-ENTITY_STATS = 'match/stats'
+ENTITY = 'match'
+ENTITY_DETAILS = f'{ENTITY}/details'
+ENTITY_TIMELINE = f'{ENTITY}/timeline'
+ENTITY_STATS = f'{ENTITY}/stats'
 SILVER = 'silver'
 GOLD = 'gold'
+FRAMES = '15'
 
-
-
-async def aggregate_match_data(data_path='.', match_id='EUW1_7005986832'):
-    #data_path = await Variable.get('data_path')
-    #data_path = data_path.value
+@task
+async def aggregate_match_data(data_path='.', match_id='EUW1_7007516257'):
+    data_path = await Variable.get('data_path')
+    data_path = data_path.value
 
     df_details  = pd.read_parquet(f"{data_path}/{SILVER}/{ENTITY_DETAILS}/{match_id}.parquet")
     df_timeline = pd.read_parquet(f"{data_path}/{SILVER}/{ENTITY_TIMELINE}/{match_id}.parquet")
@@ -21,6 +24,7 @@ async def aggregate_match_data(data_path='.', match_id='EUW1_7005986832'):
 
     roles_by_id = {int(k): v.lower() for k,v in df_details.iloc[0, 5:-1].to_dict().items()}
 
+    # Metadata
     final_df = pd.DataFrame([{'match_id': match_id}])
     final_df['match_id'] =      str(match_id)
     final_df['gameCreation'] =  df_details['gameCreation']
@@ -28,8 +32,19 @@ async def aggregate_match_data(data_path='.', match_id='EUW1_7005986832'):
     final_df['gameMode'] =      df_details['gameMode']
     final_df['gameType'] =      df_details['gameType']
     final_df['gameVersion'] =   df_details['gameVersion']
+
+    # Target
     final_df['red_winner'] =    df_details['100_winner']
     final_df['blue_winner'] =   ~final_df['red_winner']
+
+    # Neutral Objectives
+    for team, team_id in (('red', 100), ('blue', 200)):
+        final_df[f'{team}_dragons'] = df_timeline.query(f'objective == "DRAGON" and team_id == {team_id}')['objective'].count()
+        final_df[f'{team}_grubs'] = df_timeline.query(f'objective == "HORDE" and team_id == {team_id}')['objective'].count()
+        final_df[f'{team}_towers_top_taken'] = df_timeline.query(f'type == "BUILDING_KILL" and objective == "TOP_LANE TOWER_BUILDING" and team_id == {team_id}')['objective'].count()
+        final_df[f'{team}_towers_middle_taken'] = df_timeline.query(f'type == "BUILDING_KILL" and objective == "MID_LANE TOWER_BUILDING" and team_id == {team_id}')['objective'].count()
+        final_df[f'{team}_towers_bottom_taken'] = df_timeline.query(f'type == "BUILDING_KILL" and objective == "BOT_LANE TOWER_BUILDING" and team_id == {team_id}')['objective'].count()
+
 
     # Add player stats to the final dataframe
     for player_id, role in roles_by_id.items():
@@ -48,8 +63,13 @@ async def aggregate_match_data(data_path='.', match_id='EUW1_7005986832'):
         final_df[f'{prefix}_{role}_deaths'] = df_timeline.query(f'type == "CHAMPION_KILL" and objective == "{player_id}"')['objective'].count()
         final_df[f'{prefix}_{role}_assists'] = df_timeline.query('type == "CHAMPION_KILL"')['assists'].apply(lambda assists: player_id in assists).sum()
 
+    final_df.to_parquet(f'{data_path}/{GOLD}/{ENTITY}/{FRAMES}/{match_id}.parquet')
 
-    return final_df
+    return final_df, (df_timeline, df_details, df_stats)
 
 if __name__ == '__main__':
-    df = await aggregate_match_data()
+    timelines = os.listdir('./silver/match/timeline')
+    # for timeline in timelines:
+    #     match_id = timeline.split('.parquet')[0]
+    #     print(match_id)
+    #     await aggregate_match_data(data_path='.', match_id=match_id)
